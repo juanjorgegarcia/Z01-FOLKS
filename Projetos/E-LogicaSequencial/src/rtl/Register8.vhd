0b48394955aa9686cec7d8779a7655d44b85f34c
arthurolga <arthur_olga@hotmail.com>
2018-03-28 00:19:47 -0300

    Register8

--- Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
@@ -13,3 +13,29 @@ entity Register8 is
 		output: out STD_LOGIC_VECTOR(7 downto 0)
 	);
 end entity;
+
+architecture arch of Register8 is
+--signal C,L : STD_LOGIC;
+--signal D,Q : STD_LOGIC_VECTOR(7 downto 0);
+
+component BinaryDigit is
+ port(
+	clock:   in STD_LOGIC;
+	input:   in STD_LOGIC;
+	load:    in STD_LOGIC;
+	output: out STD_LOGIC
+ );
+end component;
+
+begin
+BD0: BinaryDigit port map (clock, input(0), load, output(0));
+BD1: BinaryDigit port map (clock, input(1), load, output(1));
+BD2: BinaryDigit port map (clock, input(2), load, output(2));
+BD3: BinaryDigit port map (clock, input(3), load, output(3));
+BD4: BinaryDigit port map (clock, input(4), load, output(4));
+BD5: BinaryDigit port map (clock, input(5), load, output(5));
+BD6: BinaryDigit port map (clock, input(6), load, output(6));
+BD7: BinaryDigit port map (clock, input(7), load, output(7));
+
+
+end architecture;
