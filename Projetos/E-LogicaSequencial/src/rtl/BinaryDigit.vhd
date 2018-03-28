00b177a9577797dfd6af03f17dd4ef69f2056868
JoseFernandoInsper <josefmc@al.insper.edu.br>
2018-03-28 16:39:30 -0300

    O merge ficou bugado
    repetiu o enunciado do binary digit

--- Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
@@ -14,22 +14,6 @@ entity BinaryDigit is
 	);
 end entity;
 
--- Elementos de Sistemas
--- by Luciano Soares
--- BinaryDigit.vhd
-
-Library ieee; 
-use ieee.std_logic_1164.all;
-  
-entity BinaryDigit is
-	port(
-		clock:   in STD_LOGIC;
-		input:   in STD_LOGIC;
-		load:    in STD_LOGIC;
-		output: out STD_LOGIC
-	);
-end entity;
-
 ----------------------------
 -- Implementacao do bloco --
 ----------------------------
