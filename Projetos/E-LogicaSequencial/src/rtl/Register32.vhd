f8a714c9ccfbc75005712585119afd519c0e611b
arthurolga <arthur_olga@hotmail.com>
2018-03-28 15:45:23 -0300

    register 32 vhd

--- Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
@@ -13,3 +13,26 @@ entity Register32 is
 		output: out STD_LOGIC_VECTOR(31 downto 0)
 	);
 end entity;
+
+architecture rtl of Register32 is
+
+component Register16 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(15 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
+
+signal r1out, r2out : STD_LOGIC_VECTOR(15 downto 0);
+
+begin
+
+	R8_1 : Register16 port map (clock, input(15 downto 0), load, r1out);
+	R8_2 : Register16 port map (clock, input(31 downto 16), load, r2out);
+
+	output <= r1out & r2out;
+	
+end rtl;
+
