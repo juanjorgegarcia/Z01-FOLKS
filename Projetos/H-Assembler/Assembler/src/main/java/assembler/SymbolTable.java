7248d960af857cea54de92b23a3dd34d9db806b6
JoseFernandoInsper <josefmc@al.edu.br>
2018-05-06 00:18:14 -0300

    symboltable

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -5,16 +5,43 @@
 
 package assembler;
 
+import java.util.HashMap;
+
 /**
  * Mantém uma tabela com a correspondência entre os rótulos simbólicos e endereços numéricos de memória.
  */
 public class SymbolTable {
-
+	
+	HashMap<String, Integer> symbolT = new HashMap<String, Integer>();
     /**
      * Cria a tabela de símbolos.
      */
     public SymbolTable() {
 
+    	symbolT.put("R0", 0);
+    	symbolT.put("R1", 1);
+    	symbolT.put("R2", 2);
+    	symbolT.put("R3", 3);
+    	symbolT.put("R4", 4);
+    	symbolT.put("R5", 5);
+    	symbolT.put("R6", 6);
+    	symbolT.put("R7", 7);
+    	symbolT.put("R8", 8);
+    	symbolT.put("R9", 9);
+    	symbolT.put("R10", 10);
+    	symbolT.put("R11", 11);
+    	symbolT.put("R12", 12);
+    	symbolT.put("R13", 13);
+    	symbolT.put("R14", 14);
+    	symbolT.put("R15", 15);
+    	symbolT.put("SP", 0);
+    	symbolT.put("LCL", 1);
+    	symbolT.put("ARG", 2);
+    	symbolT.put("THIS", 3);
+    	symbolT.put("THAT", 4);
+    	symbolT.put("SCREEN", 16384);
+    	symbolT.put("LED", 21184);
+    	symbolT.put("SW", 21185);
     }
 
     /**
@@ -23,7 +50,7 @@ public class SymbolTable {
      * @param  address símbolo a ser armazenado na tabela de símbolos.
      */
     public void addEntry(String symbol, int address) {
-
+    	symbolT.put(symbol, address);
     }
 
     /**
@@ -32,7 +59,7 @@ public class SymbolTable {
      * @return Verdadeiro se símbolo está na tabela de símbolos, Falso se não está na tabela de símbolos.
      */
     public Boolean contains(String symbol) {
-    	return null;
+    	return symbolT.containsKey(symbol);
     }
 
     /**
@@ -41,7 +68,8 @@ public class SymbolTable {
      * @return valor numérico associado ao símbolo procurado.
      */
     public Integer getAddress(String symbol) {
-    	return null;
+    	return symbolT.get(symbol);
+    	
     }
 
 }
